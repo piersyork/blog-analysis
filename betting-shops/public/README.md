a25d60d155c644eb0e3dca53d274fd8bffeb33d9
piersyork <piersyork@gmail.com>
2022-07-22 16:04:55 +0100

    add documentation

--- betting-shops/public/README.md
+++ betting-shops/public/README.md
@@ -1,2 +1,19 @@
-# License
+## \# Documentation
+
+## License
+
 As this data is gathered from the Open Street Map API, it must be used in accordance with the Open Street Map [license agreement](https://www.openstreetmap.org/copyright)
+
+## Variables
+
+| Variable              | Description                                                            | Source                                                | Type      |
+|-----------------------|------------------------------------------------------------------------|-------------------------------------------------------|-----------|
+| `region`              | The region of England                                                  | postcodes.io                                          | character |
+| `area_name`           | The Local Authority name                                               | postcodes.io                                          | character |
+| `area_code`           | The Local Authority ons area code                                      | postcodes.io                                          | character |
+| `n_bookies`           | The number of bookmakers within the Local Authority                    | Analysis using the Open Street Map API                | numeric   |
+| `population`          | The population size of the Local Authority                             | ONS                                                   | numeric   |
+| `deprivation`         | The Index of Multiple Deprivation average score                        | Ministry for Housing Communities and Local Government | numeric   |
+| `imd_rank`            | Where the Local Authorities deprivation score ranks                    | Ministry for Housing Communities and Local Government | numeric   |
+| `bookies_per_million` | The number of bookmakers per million people within the Local Authority | `n_bookies / population * 1e6`                        | numeric   |
+| `region_name`         | Simplified UK regions for use in the blog visualisation                | postcodes.io                                          | character |
--- testing.R
+++ testing.R
@@ -5,7 +5,7 @@ options(box.path = getwd())
 box::use(functions/theme[...])
 
 readxl::read_xlsx("~/Downloads/Industry_Statistics_November_2021.xlsx", sheet = "6a", range = "A9:O21", .name_repair = "minimal") |>
-  select(1, 9:15) |>
+  select(1:8) |>
   janitor::clean_names() |>
   mutate(year = as.numeric(stringr::word(reporting_period, 5, -1)), .before = total) |>
   select(-total, -percent_change, -reporting_period) |>
@@ -24,8 +24,49 @@ readxl::read_xlsx("~/Downloads/Industry_Statistics_November_2021.xlsx", sheet = 
         legend.position = "none",
         axis.title.y = element_blank(),
         panel.grid.major.x = element_blank()) +
-  labs(title = "While profit from horse betting is falling, profit from football \nbetting is steadily rising",
-       subtitle = "Gross gambling yield (£ millions)",
+  labs(title = "While turnover from horse betting falls, profit from football \nbetting is steadily rising",
+       subtitle = "Turnover (£ millions), years ending March",
        caption = "Source: Gambling Commission Industry Statistics, November 2021",
        x = "")
+
+gambling_yield <- readxl::read_xlsx("~/Downloads/Industry_Statistics_November_2021.xlsx", sheet = "1", range = "A8:M21", .name_repair = "minimal") |>
+  janitor::clean_names() |>
+  select(-contains("total"), -percent_change) |>
+  mutate(year = as.numeric(stringr::word(reporting_period, 5) |> stringr::str_remove("P")), .before = reporting_period,
+         across(everything(), as.double)) |>
+  select(-reporting_period) |>
+  tidyr::pivot_longer(arcades_non_remote:the_national_lottery_remote_and_non_remote,
+                      names_to = "category", values_to = "yield")
+
+gambling_yield |>
+  filter(category %in% c("betting_non_remote", "betting_remote")) |>
+  na.omit() |>
+  group_by(category) |>
+  mutate(label = ifelse(year == max(year), stringr::str_to_title(stringr::str_replace_all(category, "_", " ")), NA)) |>
+  ggplot(aes(year, yield, colour = category, label = label)) +
+  geom_line() +
+  ggrepel::geom_text_repel(hjust = 0, xlim = Inf, na.rm = TRUE, segment.colour = "grey") +
+  coord_cartesian(clip = "off") +
+  theme_piers() +
+  scale_colour_piers() +
+  scale_y_continuous(labels = scales::label_dollar(prefix = "£"), limits = c(0, NA)) +
+  scale_x_continuous(breaks = seq(2010, 2020, 2)) +
+  theme(plot.margin = margin(11, 100, 5, 10),
+        legend.position = "none",
+        axis.title.y = element_blank(),
+        panel.grid.major.x = element_blank()) +
+  labs(title = "Yield from remote betting soars as in-shop \nbetting begins its descent",
+       subtitle = "Gross gambling yield (£ millions), years ending March",
+       caption = "Source: Gambling Commission Industry Statistics, November 2021",
+       x = "")
+
+
+
+
+
+
+
+
+
+
 
