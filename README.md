4a77d05fae06f0b451983c2f58715a63d829f0f1
piersyork <piersyork@gmail.com>
2022-07-22 11:25:42 +0100

    add readme

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# Blog analysis
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+This repository stores the analysis and replication data for my personal blog at [piers.york@github.io](piers.york@github.io). Each project will have it's own readme file, outlining how the analysis was undertaken. Some projects will provide new data and documentation will be provided for this.
+
--- testing.R
+++ testing.R
@@ -1,30 +1,31 @@
+library(dplyr)
+library(ggplot2)
 
+options(box.path = getwd())
+box::use(functions/theme[...])
 
-lsoa_bookies <- as.data.table(bookies)[, .(n_bookies = .N), by = lsoa][as.data.table(lsoa_map)[, .(lsoa = LSOA11CD, lsoa_name = LSOA11NM, geometry)], on = "lsoa"]
+readxl::read_xlsx("~/Downloads/Industry_Statistics_November_2021.xlsx", sheet = "6a", range = "A9:O21", .name_repair = "minimal") |>
+  select(1, 9:15) |>
+  janitor::clean_names() |>
+  mutate(year = as.numeric(stringr::word(reporting_period, 5, -1)), .before = total) |>
+  select(-total, -percent_change, -reporting_period) |>
+  tidyr::pivot_longer(dogs:other, names_to = "category", values_to = "yield") |>
+  group_by(category) |>
+  mutate(label = ifelse(year == max(year), stringr::str_to_title(category), NA)) |>
+  ggplot(aes(year, yield, colour = category, label = label)) +
+  geom_line() +
+  ggrepel::geom_text_repel(hjust = 0, xlim = Inf, na.rm = TRUE, segment.colour = "grey") +
+  coord_cartesian(clip = "off") +
+  scale_colour_piers() +
+  scale_y_continuous(labels = scales::label_dollar(prefix = "£"), limits = c(0, NA)) +
+  scale_x_continuous(breaks = seq(2010, 2020, 2)) +
+  theme_piers() +
+  theme(plot.margin = margin(11, 55, 5, 10),
+        legend.position = "none",
+        axis.title.y = element_blank(),
+        panel.grid.major.x = element_blank()) +
+  labs(title = "While profit from horse betting is falling, profit from football \nbetting is steadily rising",
+       subtitle = "Gross gambling yield (£ millions)",
+       caption = "Source: Gambling Commission Industry Statistics, November 2021",
+       x = "")
 
-lsoa_bookies[is.na(n_bookies), n_bookies := 0]
-
-lsoa_bookies[, .(n_bookies = sum(n_bookies)), by = area_name][order(-n_bookies)]
-
-areas <- readr::read_csv("~/Downloads/Output_Area_to_Lower_Layer_Super_Output_Area_to_Middle_Layer_Super_Output_Area_to_Local_Authority_District_(December_2020)_Lookup_in_England_and_Wales.csv") |>
-  select(lsoa = LSOA11CD, area_code = LAD20CD, area_name = LAD20NM, region = RGN20NM) |>
-  distinct() |>
-  as.data.table()
-
-areas[
-  lsoa_bookies, on = "lsoa"
-][
-  , .(n_bookies = sum(n_bookies)), by = area_code
-][
-  as.data.table(uk_map), on = "area_code"
-] |>
-  st_as_sf() |>
-  qtm(fill = "n_bookies")
-
-
-lsoa_bookies[order(-n_bookies), head(.SD)]
-
-tmap_mode("view")
-lsoa_bookies |>
-  st_as_sf() |>
-  qtm(fill = "n_bookies", borders = NULL)
